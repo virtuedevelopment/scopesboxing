77d543a2ee45bd539b2670623cea77ba7e41c557
Virtue Engine <101955083+virtuedevelopment@users.noreply.github.com>
2023-12-03 21:18:53 -0500

    added landing

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -1,10 +1,15 @@
 //Landing page
 import Link from "next/link"
+import Image from "next/image"
 
 export default function Landing() {
   return (
-    <main>
-        <Link href={'/home'} className="button-01">Enter</Link>
+    <main className="landing" >
+      <Image className="landingImage" src={'/landing.png'} width={500} height={500} alt="background"/>
+        <div className="enterBox">
+          <Image src={'/logoRB.svg'} width={300} height={270} alt="logo" />
+          <Link href={'/home'} className="button-01">Enter</Link>
+        </div>
     </main>
   )
 }
