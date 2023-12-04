5a7c571da0a3c098847ee129efa5619cfe5f6a8b
Virtue Engine <101955083+virtuedevelopment@users.noreply.github.com>
2023-12-03 19:02:50 -0500

    test 1

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -1,10 +1,10 @@
 //Landing page
 import Link from "next/link"
 
-export default function Home() {
+export default function Landing() {
   return (
     <main>
-      
+        <Link href={'/home'} className="button-01">Enter</Link>
     </main>
   )
 }
